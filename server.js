cd7cf116a101b61b774cbeacae7f7fb8acbb9355
Ahmad Yahaya <yahmad2601@gmail.com>
2025-01-06 20:34:03 +0100

    Connected to Database

--- server.js
+++ server.js
@@ -1,23 +1,35 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const mongoose = require("mongoose");
 
 const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-let leaderboard = [];
+// Connect to MongoDB
+const mongoURI =
+  "mongodb+srv://yahmad2601:<6QsAAL82YqZ03NGr>@tap-catch.ibp9b.mongodb.net/?retryWrites=true&w=majority&appName=Tap-Catch";
+mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true });
 
-app.post("/submit-score", (req, res) => {
+const leaderboardSchema = new mongoose.Schema({
+  name: String,
+  department: String,
+  score: Number,
+});
+
+const Leaderboard = mongoose.model("Leaderboard", leaderboardSchema);
+
+app.post("/submit-score", async (req, res) => {
   const { name, department, score } = req.body;
-  leaderboard.push({ name, department, score });
-  leaderboard.sort((a, b) => b.score - a.score);
-  leaderboard = leaderboard.slice(0, 5);
+  const newScore = new Leaderboard({ name, department, score });
+  await newScore.save();
   res.send("Score submitted");
 });
 
-app.get("/leaderboard", (req, res) => {
-  res.json(leaderboard);
+app.get("/leaderboard", async (req, res) => {
+  const scores = await Leaderboard.find().sort({ score: -1 }).limit(5);
+  res.json(scores);
 });
 
 const PORT = process.env.PORT || 3000;
