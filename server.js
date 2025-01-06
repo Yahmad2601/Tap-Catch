48e14b3e3bb14c444329798617249a696c8997ed
Ahmad Yahaya <yahmad2601@gmail.com>
2025-01-06 22:29:35 +0100

    Updated the backend to use MongoDB for persistent storage3

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(cors());
 
 // Connect to MongoDB
 const mongoURI =
-  "mongodb+srv://yahmad2601:<6QsAAL82YqZ03NGr>@tap-catch.ibp9b.mongodb.net/?retryWrites=true&w=majority&appName=Tap-Catch";
+  "mongodb+srv://yahmad2601:<Z5rBn3RLSAO50JLw>@tap-catch.ibp9b.mongodb.net/?retryWrites=true&w=majority&appName=Tap-Catch";
 mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 const leaderboardSchema = new mongoose.Schema({
